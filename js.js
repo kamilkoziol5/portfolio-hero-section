e6b759dd0271f8fb7177bd09c4a66b6b246ed6e4
Kamil <kamilkoziol529@gmail.com>
2025-02-01 11:46:09 +0100

    first commit

---
+++ js.js
@@ -0,0 +1,9 @@
+// Funkcja do otwierania menu mobilnego
+function openSidebar() {
+    document.querySelector('.mobile-menu').classList.add('open');
+}
+
+// Funkcja do zamykania menu mobilnego
+function closeSidebar() {
+    document.querySelector('.mobile-menu').classList.remove('open');
+}
